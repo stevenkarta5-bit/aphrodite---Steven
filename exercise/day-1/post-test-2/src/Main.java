43c704166a184144145b02f193e9a488ce2eb3a5
stevenkarta5-bit <stevenkarta5@gmail.com>
2025-09-03 19:39:02 +0700

    pengerjaan post test 2

---
+++ exercise/day-1/post-test-2/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int totalBiaya = 0;
+
+        System.out.println("Input Jenis Kendaraan");
+        String kendaraan = input.nextLine();
+
+        System.out.println("Input Lama parkir (per-jam)");
+        int lamaParkir = input.nextInt();
+
+        if (kendaraan.equals("motor")) {
+            if (lamaParkir <= 2){
+                totalBiaya = 2000;
+            } else {
+                totalBiaya = 2000 + (lamaParkir - 2) * 1000;
+            }
+
+            if (totalBiaya > 10000) {
+                totalBiaya = 10000;
+            }
+
+            
+        } else if (kendaraan.equals("mobil")) {
+            if (lamaParkir <= 2) {
+                totalBiaya = 5000;
+            } else {
+                totalBiaya = 5000 + (lamaParkir - 2) * 2000;
+            }
+
+            if (totalBiaya > 25000) {
+                totalBiaya = 25000;
+            }
+        } else {
+            System.out.println("Tidak termasuk kategori");
+            input.close();
+            return;
+        }
+
+        System.out.println("- " + kendaraan + ", " + lamaParkir + ", " + totalBiaya);
+
+
+    }
+}
