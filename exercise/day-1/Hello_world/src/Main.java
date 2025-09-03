ad7de32073ddfb886bc3deb4009e14f5a0229f60
stevenkarta5-bit <stevenkarta5@gmail.com>
2025-09-03 15:53:11 +0700

    pengerjaan exercise day-1

---
+++ exercise/day-1/Hello_world/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     //   System.out.println("Hello world");
+
+    int myNumber = 10;
+    long myAngkaPanjang = 12394;
+    double myDecimal = 0.3;
+    boolean msukIT = true;
+    String namaSaya = "Steven Kartawinata";
+    int[] numbers = {1, 2, 3, 4};
+
+    System.out.println(myNumber);
+        System.out.println(myAngkaPanjang);
+        System.out.println(myDecimal);
+        System.out.println(msukIT);
+        System.out.println(namaSaya);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
