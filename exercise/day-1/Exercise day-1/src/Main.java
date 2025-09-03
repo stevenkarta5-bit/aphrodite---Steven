cba7ee1498e41bc0a46f2918577e8b304ce22a08
stevenkarta5-bit <stevenkarta5@gmail.com>
2025-09-03 16:41:40 +0700

    pengerjaan tugas 2

---
+++ exercise/day-1/Exercise day-1/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Soal 1 Cerita Kue Andi
+        int kueAndi = 12-4;
+
+        // Soal 2
+        int ongkirSetelahDiscount = 25000/2;
+
+        // Soal 3
+        int sisaPermenSinta = 20%6;
+
+        // Soal 4
+        int pointPemain = 80;
+        pointPemain =+ 15;
+
+        // Soal 5
+        int umurBudi = 16;
+        boolean result = umurBudi >= 18;
+        System.out.println(result);
+    }
+}
